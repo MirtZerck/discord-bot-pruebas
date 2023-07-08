98b51e4458f020e37118e56bc7b98d57fe4f4b46
MirtZerck <hinosmichael@gmail.com>
2023-07-08 18:47:51 -0500

    Mensaje de espera en comando Meme

--- commands/meme.js
+++ commands/meme.js
@@ -7,15 +7,15 @@ export const sendMemeCommand = {
 
   async execute(message, args)  { 
 
-    const momazo = await obtenerMeme();
+    /* const momazo = await obtenerMeme(); */
     
     const embedMeme = new MessageEmbed()
       .setAuthor(
         "Gatos Gatunos",
         "https://fotografias.lasexta.com/clipping/cmsimages02/2019/01/25/DB41B993-B4C4-4E95-8B01-C445B8544E8E/98.jpg?crop=4156,2338,x0,y219&width=1900&height=1069&optimize=high&format=webply"
       )
-      .setTitle(`Ríanse por favor :C`)
-      .setDescription(momazo)
+      .setTitle(`Trabajando en el comando`)
+      .setDescription('Próximamente')
       .setColor("#81d4fa")
       .setTimestamp();
 
--- utils/api_meme.js
+++ utils/api_meme.js
@@ -2,6 +2,5 @@ export async function obtenerMeme(){
     const url = 'https://api.imgflip.com/get_memes'
     const respuesta = await fetch(url)
     const data = await respuesta.json();
-    console.log(data);
-    return data;
+    return data.data.memes[0];
 } 
