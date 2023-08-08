5d93f2c1c5851c036b8ce8f9907363cad95585e1
MirtZerck <hinosmichael@gmail.com>
2023-08-08 18:50:34 -0500

    Ahora los puntos ganados por mensaje para el ranking server son aleatorios entre 3 y 5 (respuestas.js línea 102)

--- commands/respuestas.js
+++ commands/respuestas.js
@@ -99,7 +99,8 @@ export const onMessageCreate = async (client) => {
         const user = rankt1[message.author.id];
         // Si ha pasado 1 minuto
         if (timestamp - user.last >= 60 * 1000) {
-          user.puntos = user.puntos + 1;
+          const puntosGanados = Math.floor(Math.random() * 3) + 3;
+          user.puntos += puntosGanados;
           user.last = timestamp;
           setUserRankTabla1(message.author.id, user);
         }
