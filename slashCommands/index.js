58eced7d4ed8446f7e69cfaa03ba581f94bbca23
MirtZerck <hinosmichael@gmail.com>
2024-03-05 14:30:12 -0500

    Deshabilitado comando Refresh

--- slashCommands/index.js
+++ slashCommands/index.js
@@ -1,4 +1,4 @@
 import { helpSlashCommands } from "./help.js";
 import { reloadCommand } from "./refreshCommands.js";
 
-export const arraySlashCommands = [helpSlashCommands, reloadCommand];
+export const arraySlashCommands = [helpSlashCommands, /* reloadCommand */];
