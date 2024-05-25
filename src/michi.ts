a822fecd3acd78c86e538223c343d47ffe4f3bb9
MIRTZERCK <hinosmichael@gmail.com>
2024-05-25 15:12:52 -0500

    Añadida función para reproducir PlayList en Music.

--- src/michi.ts
+++ src/michi.ts
@@ -15,8 +15,8 @@ import { onInteractionCreate } from "./slashCommands/interactionCreate.js";
 
 dotenv.config();
 
-export const token = process.env.TOKEN2!;
-export const APPLICATION_ID = process.env.APPLICATION_ID2!;
+export const token = process.env.TOKEN!;
+export const APPLICATION_ID = process.env.APPLICATION_ID!;
 
 const firebaseConfig = JSON.parse(process.env.FIREBASE_ADMIN_SDK!);
 
